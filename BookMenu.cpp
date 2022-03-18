a98858142afefbc339c6af981ffc2c661d133ad6
denda0duong <trandangkhoa210802@gmail.com>
2022-03-19 00:21:39 +0700

    add BookMenu

---
+++ BookMenu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+struct BookInfo {
+	char* maSach = new char[7];
+	char* tenSach = new char]50];
+	char* tacGia = new char[50];
+	char* nhaXuatBan = new char[50];
+	int giaBan, soTrang, namXuatBan;
+}
+
+int main() {
+
+	return 0;
+}

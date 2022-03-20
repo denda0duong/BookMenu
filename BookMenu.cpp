c12075e31e15dd16ff67cf3cfeecb0056963c9f8
denda0duong <trandangkhoa210802@gmail.com>
2022-03-20 21:36:33 +0700

    Done

--- BookMenu.cpp
+++ BookMenu.cpp
@@ -11,10 +11,36 @@ struct BookInfo {
 	int giaBan, soTrang, namXuatBan;
 };
 
+int kiemTraMaSach(char* k) {
+	int n = strlen(k) + 1;
+	//kiem tra xem do dai ma sach co lon hon 7 khong
+	if (n > 7) {
+		return 0;
+	}
+	else {
+		//kiem tra xem trong ma sach co khoang trang khong
+		for (int i = 0; i < n; i++) {
+			if (k[i] == '\r' || k[i] == '\n' || k[i] == ' ') {
+				return 0;
+			}
+		}
+	}
+
+	return 1; //thoa yeu cau
+}
 void nhapThongTinQuyenSach(BookInfo* k) {
 	cout << "Dien vao ma sach (toi da 7 ky tu, khong khoang trang): ";
 	cin.ignore();
 	cin.getline(k->maSach, 99);
+	if (kiemTraMaSach(k->maSach) == 0) {
+		do {
+			cout << "Ma sach khong hop le! Vui long nhap lai.\n";
+			cout << "Dien vao ma sach (toi da 7 ky tu, khong khoang trang): ";
+			cin.getline(k->maSach, 99);
+		} while (kiemTraMaSach(k->maSach) == 0);
+	}
+	else
+		cout << "Ma sach hop le!\n";
 	cout << "Dien vao ten sach: ";
 	cin.getline(k->tenSach, 99);
 	cout << "Dien vao ho ten tac gia: ";
@@ -142,45 +168,34 @@ void themSach(BookInfo*& danhSach, int n) {
 	}
 }
 
-//ham kiem tra va dem so quyen sach co NXB truoc nam chi dinh
-int kiemTraNXB(BookInfo* danhSach, int year, int n) {
-	int count = 0;
+int xoaSach(BookInfo*& danhSach, int year, int& n) {
+	int found = 0, clone = n;
+	int count = 1;
+
 	for (int i = 0; i < n; i++) {
 		if (danhSach[i].namXuatBan < year) {
+			found = 1;
+			clone--;
+			cout << "Da xoa " << count << " quyen sach!\n";
 			count++;
+			for (int j = i; j < n; j++) {
+				danhSach[j] = danhSach[j + 1];
+			}
+			n--;
 		}
 	}
-	return count;
-}
 
-int xoaSach(BookInfo*& danhSach, int year, int n) {
-	if (kiemTraNXB(danhSach, year, n) == 0) {
-		cout << "Khong co quyen sach nao thoa dieu kien.\n";
+	if (found == 0) {
+		cout << "Khong co quyen sach nao thoa dieu kien!\n";
 		return 0;
 	}
-	else {
-		int j = 0;
-		//tao mang chua danh sach cac quyen sach co nam xuat ban sau nam chi dinh
-		BookInfo* danhSach03 = new BookInfo[n + 20];
-		for (int i = 0; i < n; i++) {
-			if (danhSach[i].namXuatBan >= year) {
-				danhSach03[j] = danhSach[i];
-				j++;
-			}
-		}
-		//cap phat dong lai
-		delete[] danhSach;
-		danhSach = new BookInfo[j + 20];
-		for (int i = 0; i < j; i++) {
-			danhSach[i] = danhSach03[i];
-		}
+	else
 		return 1;
-	}
 }
 
 int main() {
-	int soLuongSach, key, k, year;
-	BookInfo* danhSach = new BookInfo[50];
+	int soLuongSach = 0, key, k, year;
+	BookInfo* danhSach = new BookInfo[soLuongSach + 20];
 
 	cout << "MENU QUAN LY DANH SACH CAC QUYEN SACH\n";
 	cout << "---\n";
@@ -225,7 +240,7 @@ int main() {
 		case 4:
 			if (soLuongSach > 0) {
 				cout << "\n4. Tim sach.\n";
-				cout << "Nhap k: ";
+				cout << "Nhap k (so trang ban yeu cau): ";
 				cin >> k;
 				cout << "Cac quyen sach co it hon " << k << " trang va co gia ban cao nhat:\n";
 				timSach(danhSach, k, soLuongSach);
@@ -241,7 +256,9 @@ int main() {
 				soLuongSach++;
 			}
 			else {
-				cout << "Danh sach trong!\n";
+				soLuongSach = 1;
+				nhapDanhSach(danhSach, soLuongSach);
+				cout << "Them 1 quyen sach thanh cong!\n";
 			}
 			break;
 		case 6:
@@ -249,10 +266,9 @@ int main() {
 				cout << "\n6. Xoa sach.\n";
 				cout << "Nhap vao nam xuat ban: ";
 				cin >> year;
-				xoaSach(danhSach, year, soLuongSach);
+
 				if (xoaSach(danhSach, year, soLuongSach) == 1) {
 					cout << "Cac quyen sach co nam xuat ban truoc " << year << " da duoc xoa thanh cong!\n";
-					soLuongSach = soLuongSach - kiemTraNXB(danhSach, year, soLuongSach);
 				}
 			}
 			else {
